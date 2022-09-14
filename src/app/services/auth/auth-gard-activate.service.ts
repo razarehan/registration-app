d5a0664c33951996d8999aae226135d3b641c6b3
BdSwami <badal.swami360@gmail.com>
2022-09-14 12:31:57 +0530

    eror send to rehan

---
+++ src/app/services/auth/auth-gard-activate.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthGardService } from './auth-gard.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGardActivateService implements CanActivate {
+
+  constructor(private authGardService: AuthGardService,
+    private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.authGardService.isAuthenticate().then(
+      (authenticated: boolean) => {
+        if(authenticated) {
+          return true;
+        }
+        else {
+          this.router.navigate(['login']);
+        }
+      }
+    );
+  }
+}
